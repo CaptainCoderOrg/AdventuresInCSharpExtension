4c87475fca3a58caacb11aa97cabf33b6f307e95
Joseph Collard <jcollard@users.noreply.github.com>
2022-09-04 10:59:25 -0700

    feat: Add Generate URL and Preview functions.

--- adventures-in-c--extension/src/extension.ts
+++ adventures-in-c--extension/src/extension.ts
@@ -1,8 +1,14 @@
+// https://share.captaincoder.org/?id=b3006e60-64f8-4a4b-a663-fdfad78ee452
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import { Base64 } from 'js-base64';
+import * as axios from 'axios';
+
+// Load Shared Program
+// vscode://captain-coder.adventures-in-c--extension/load-shared-program?id=d5914972-fbda-46ff-9d66-81e96ad7ef67
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,8 +20,17 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 	context.subscriptions.push(disposable);
 
+	disposable = vscode.commands.registerCommand('adventures-in-c--extension.previewProgram', () => {
+		openSimpleProgram();
+	});
+	context.subscriptions.push(disposable);
 
-	// vscode://undefined_publisher.adventures-in-c--extension/some-data
+	disposable = vscode.commands.registerCommand('adventures-in-c--extension.generateProgramURL', () => {
+		vscode.window.showInformationMessage('Generating Program URL');
+		generateProgramURL();
+	});
+	context.subscriptions.push(disposable);
+
 	disposable = vscode.window.registerUriHandler(new URIHandler());
 	context.subscriptions.push(disposable);
 
@@ -28,16 +43,7 @@ export function deactivate() {}
 
 function runProgram() {
 	const adventuresPath = getExtensionPath();
-	const simpleProjectProgram = getExtensionPath() + "/SimpleProject/Program.cs";
-	if(!fs.existsSync(simpleProjectProgram)){
-		vscode.window.showInformationMessage("No Program Found.");
-		return;
-	}
-
-	vscode.workspace.openTextDocument(simpleProjectProgram).then(doc => {
-		let options = { preview: true };
-		vscode.window.showTextDocument(doc, options);
-	});
+	openSimpleProgram();
 
 	let terminal = vscode.window.terminals.find(t => t.name=== "Adventures in C#");
 	if (terminal !== undefined) {
@@ -50,9 +56,27 @@ function runProgram() {
 	terminal.sendText("dotnet run --project SimpleProject/SimpleProject.csproj");
 }
 
-function loadAndRunSimpleProgram(programData : string) {
+function openSimpleProgram() {
+	const simpleProjectProgram = getExtensionPath() + "/SimpleProject/Program.cs";
+	if(!fs.existsSync(simpleProjectProgram)){
+		vscode.window.showInformationMessage("No Program Found.");
+		return;
+	}
+
+	vscode.workspace.openTextDocument(simpleProjectProgram).then(doc => {
+		let options = { preview: true };
+		vscode.window.showTextDocument(doc, options);
+	});
+}
+
+function loadSimpleProgram(programData : string) {
 	const simpleProjectProgram = getExtensionPath() + "/SimpleProject/Program.cs";
 	fs.writeFileSync(simpleProjectProgram, programData);
+	openSimpleProgram();
+}
+
+function loadAndRunSimpleProgram(programData : string) {
+	loadSimpleProgram(programData);
 	runProgram();
 }
 
@@ -70,6 +94,31 @@ function setupExtension() {
 	}
 }
 
+function generateProgramURL() : void {
+	const editor = vscode.window.activeTextEditor;
+	if (editor === undefined) {
+		vscode.window.showErrorMessage("No source code editor selected.");
+		return;
+	}
+	const fileUri = vscode.Uri.parse(editor.document.fileName);
+	const paths = fileUri.path.split('\\');
+	const fileName = paths[paths.length-1];
+
+	if (fileName !== "Program.cs") {
+		vscode.window.showErrorMessage("Generating program URLs only works for Program.cs files.");
+		return;
+	}
+	const base64Program = Base64.encode(editor.document.getText());
+	const uri = `https://us-central1-introtocsharp-a5eeb.cloudfunctions.net/createLoadProgramURL?programData=${base64Program}`;
+	axios.default.get(uri).then(response => {
+		const options = { title: "Share URL", value: response.data };
+		vscode.window.showInputBox(options);
+	})
+	.catch(error => {
+		vscode.window.showErrorMessage(`Could not generate URL: ${error}`)
+	});
+}
+
 function getExtensionPath() : string {
 	return vscode.extensions.getExtension("captain-coder.adventures-in-c--extension")!.extensionPath;
 }
@@ -81,6 +130,18 @@ class URIHandler implements vscode.UriHandler {
 		if (uri.path === "/load-simple-project") {
 			this.loadSimpleProject(uri.query);
 		}
+		else if (uri.path === "/load-shared-program") {
+			this.loadSharedProject(uri.query);
+		}
+	}
+
+	private loadSharedProject(query : string) {
+		if(!query.startsWith("program.cs=")) {
+			vscode.window.showErrorMessage("Unable to load program.");
+			return;
+		}
+		const programData = Base64.decode(query.substring(11));
+		loadSimpleProgram(programData);
 	}
 
 	private loadSimpleProject(query : string) {
